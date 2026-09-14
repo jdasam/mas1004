cc42b4161c82ec527f4566bbdd390016cb2bc965
Dasaem Jeong <dasaemj@sogang.ac.kr>
2026-09-14 15:06:36 +0900

    Publish demo: layer-by-layer-2d

--- demos/layer-by-layer-2d/test/load.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-module.exports = function loadCore(){
-  const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
-  const m = html.match(/<script id="lb-core">([\s\S]*?)<\/script>/);
-  if(!m) throw new Error('lb-core script not found');
-  return (0, eval)(m[1] + '\n;LB');
-};
